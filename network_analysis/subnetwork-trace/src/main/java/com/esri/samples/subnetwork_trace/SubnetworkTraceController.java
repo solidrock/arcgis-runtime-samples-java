8d9c17b80fd82dc84a6480031d3caa5ffe173f2e
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-11-15 10:57:22 +0000

    whitespace

--- network_analysis/subnetwork-trace/src/main/java/com/esri/samples/subnetwork_trace/SubnetworkTraceController.java
+++ network_analysis/subnetwork-trace/src/main/java/com/esri/samples/subnetwork_trace/SubnetworkTraceController.java
@@ -434,8 +434,8 @@ public class SubnetworkTraceController {
                       // create query parameters to find features who's network source name matches the layer's feature
                       // table name
                       QueryParameters queryParameters = new QueryParameters();
+                      
                       utilityElementTraceResult.getElements().forEach(utilityElement -> {
-
                         String networkSourceName = utilityElement.getNetworkSource().getName();
                         String featureTableName = ((FeatureLayer) layer).getFeatureTable().getTableName();
 
